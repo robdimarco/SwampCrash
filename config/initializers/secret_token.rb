d70eb549fba1383b3c813e24e5088b50840f093a
Rob Di Marco <rob@innovationontherun.com>
2011-03-25 12:33:21 -0400

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swampcrash::Application.config.secret_token = 'a21fc142c74f68e3bb9c0958b3ae0c7a5b234aeb257924f45b917ceda8c051d9844523c6d3b4b00d4008d0390d3286208915a5cbc125bf8d64aa6c10d69f0516'
