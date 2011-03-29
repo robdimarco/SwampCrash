2665b8dc128c395b271356545c92ec8ea9ed1b09
Rob Di Marco <rob@innovationontherun.com>
2011-03-29 17:07:10 -0400

    Parameterize title

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,6 +1,9 @@
 class Quiz < ActiveRecord::Base
   belongs_to :owner, :class_name=>"User", :foreign_key=>"owner_id"
   has_many :quiz_questions, :order=>"position"
+  def to_param
+    "#{id}-#{name.parameterize}"
+  end
 end
 
 
