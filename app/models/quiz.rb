0f968ed9406412b7e1bebadc83a05ab9328e9b01
Rob Di Marco <rob@innovationontherun.com>
2011-03-29 20:07:26 -0400

    Added from http://html5boilerplate.com/

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,6 +1,7 @@
 class Quiz < ActiveRecord::Base
   belongs_to :owner, :class_name=>"User", :foreign_key=>"owner_id"
   has_many :quiz_questions, :order=>"position"
+  has_many :questions, :through=>:quiz_questions
   def to_param
     "#{id}-#{name.parameterize}"
   end
