ce245e818c900caae23cdbc3ff06409875f10ed9
Rob Di Marco <rob@innovationontherun.com>
2011-04-12 14:17:19 -0400

    Still refactoring view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def blueprint_container
+    params[:show_grid] == 'true' ? 'container showgrid' : 'container'
+  end
+  def menu_tab_link(label, url)
+    content_tag(:li, link_to( label, url, {:class=>(current_page?(url) ? 'selected' : "")}))
+  end
 end
