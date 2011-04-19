2b75c48c5dba2d65dab399d6ebe3fdab9633af64
Rob Di Marco <rob@innovationontherun.com>
2011-04-19 19:32:39 -0400

    Tweaking of headers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,7 @@ module ApplicationHelper
   def menu_tab_link(label, url)
     content_tag(:li, link_to( label, url, {:class=>(current_page?(url) ? 'selected' : "")}))
   end
+  def page_title(default_title)
+    content_for(:title).blank? ? default_title : content_for(:title)
+  end
 end
