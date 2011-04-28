023f8ed39e90e6a110f9889c9a7cceca18738d7f
Rob Di Marco <rob@innovationontherun.com>
2011-04-28 10:28:54 -0400

    Added contact form

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,2 +1,10 @@
 class ContentController < ApplicationController
+  def contact_us
+    @contact_us = ContactUs.new(params[:contact_us])
+    if request.post? and @contact_us.valid?
+      ContactUsMailer.contact_us(@contact_us).deliver
+      flash[:notice] = "Thanks for your inquiry, we will get back to you shortly"
+      redirect_to content_path(:action=>:contact_us) and return
+    end
+  end
 end
--- app/models/contact_us.rb
+++ app/models/contact_us.rb
@@ -6,10 +6,10 @@ class ContactUs
   attr_accessor :name, :email, :subject, :message
   validates_presence_of :name, :email, :message, :subject
   validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\z/
-  def initialize(attributes = {})
+  def initialize(attributes=nil)
     attributes.each do |name, value|
       send("#{name}=", value)
-    end
+    end unless attributes.nil?
   end
 
   def persisted?
