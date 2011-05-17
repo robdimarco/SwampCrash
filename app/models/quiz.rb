61d76d8cabdcec16ea71711b697d13c69ee11ab0
Rob Di Marco <rob@innovationontherun.com>
2011-05-17 11:38:06 -0400

    Refactored state machine

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,30 +1,44 @@
 require 'scorecard'
 class Quiz < ActiveRecord::Base
-  include AASM
-  VALID_STATUS ||= %w( pending active complete )
-  aasm_column :status
-  aasm_initial_state :pending
-  VALID_STATUS.each{|st| 
-    aasm_state st.to_sym
-  }
-  
-  aasm_event :publish do
-    transitions :to=>:active, :from=>:pending
-  end
-  aasm_event :complete do
-    transitions :to=>:complete, :from=>:active
-  end
-  
-  scope :active, where(:status => "active")
-  scope :completed, where(:status => "complete").order("updated_at").reverse_order
-  scope :released_publicly, where(:status => ["active", "complete"]).order("updated_at").reverse_order
-  
   belongs_to :owner, :class_name=>"User", :foreign_key=>"owner_id"
   has_many :quiz_questions, :order=>"position"
   has_many :questions, :through=>:quiz_questions, :order=>"position"
   has_many :answer_sheets
   attr_accessible :name, :description
 
+  
+  module StateMachine
+    def self.included(base)
+      base.send :include, AASM
+      base.class_eval do
+        #
+        # State transitions
+        #
+        aasm_column :status
+        aasm_initial_state :pending
+        %w( pending active complete ).each{|st| 
+          aasm_state st.to_sym
+        }
+  
+        aasm_event :publish, :after=>:after_publish do
+          transitions :to=>:active, :from=>:pending
+        end
+        aasm_event :complete, :after=>:after_complete do
+          transitions :to=>:complete, :from=>:active
+        end
+        def after_publish
+        end
+        def after_complete
+        end
+      end
+    end
+  end
+  include Quiz::StateMachine
+  
+  scope :active, where(:status => "active")
+  scope :completed, where(:status => "complete").order("updated_at").reverse_order
+  scope :released_publicly, where(:status => ["active", "complete"]).order("updated_at").reverse_order
+  
   #
   # Current score in a hash with keys as question ids.  For each question id key, the value is a hash
   # with keys of the answer IDs and values of the count of people with that answer
