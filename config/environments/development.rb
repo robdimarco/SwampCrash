9bd05335ed0006618ba70ffef3621c0e5a176c16
Rob Di Marco <rob@innovationontherun.com>
2011-06-10 10:41:01 -0400

    Do not send out mails in dev.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,7 @@ Swampcrash::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
+  config.action_mailer.delivery_method = :test
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
