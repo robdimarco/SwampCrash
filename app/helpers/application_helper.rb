52041d51e84cc0b7e9b90f3d87e52d0769991644
Rob Di Marco <rob.dimarco@416software.com>
2012-12-29 18:03:52 -0500

    Moved home page and app layout to TB

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-  def blueprint_container
-    params[:show_grid] == 'true' ? 'container showgrid' : 'container'
-  end
-  def menu_tab_link(label, url)
-    content_tag(:li, link_to( label, url, {:class=>(current_page?(url) ? 'selected' : "")}))
-  end
-  def page_title(default_title)
-    content_for(:title).blank? ? default_title : content_for(:title)
-  end
 end
