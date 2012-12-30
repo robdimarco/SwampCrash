169d7fd393b74c89f86ba0983c3698a1fbd7fc99
Rob Di Marco <rob.dimarco@416software.com>
2012-12-30 14:39:12 -0500

    Do not initialize on precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Swampcrash::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.initialize_on_precompile = false
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
